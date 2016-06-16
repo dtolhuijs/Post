3423719827ef6c4fa18f3ab803d561ae87afd11c
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-16 11:42:08 +0200

    Form to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find( params[:id])
   end
+
+  def create
+     post_params = params.require( :post ).permit( :content )
+
+     @post = Post.new( content: post_params[:content] )
+
+     if @post.save
+        redirect_to posts_path
+     else
+        render posts_path
+     end
+  end
 end
